6fbcdc6e16c6f18b4a9027e04c32e2902938ac51
Anran Zhang <zhanganran2011@Live.com>
2018-06-06 15:27:36 -0300

    Commit stage 1 changes

---
+++ Validator.java
@@ -0,0 +1,11 @@
+
+public class Validator {
+
+	public boolean validate(String pass) {
+		if(pass.equals("password"))
+			return false;
+		if(pass.length()<=8)
+			return false;
+		return true;
+	}
+}
